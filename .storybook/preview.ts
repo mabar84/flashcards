9956871e971131cc6e9fec5bc65abc4fa9ed3684
mabar84 <mabar@tut.by>
2024-04-28 22:30:37 +0300

    errors in vercel

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,9 @@
-import type { Preview } from '@storybook/react'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/700.css'
-import '../src/styles/index.scss'
+import '@/styles/index.scss';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/700.css';
+
+import type { Preview } from '@storybook/react';
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -11,7 +13,20 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#000000',
+        },
+        {
+          name: 'light',
+          value: '#ffffff',
+        },
+      ],
+    },
   },
-}
+};
 
-export default preview
+export default preview;
