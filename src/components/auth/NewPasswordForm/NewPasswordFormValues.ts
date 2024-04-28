77ca4d721dc95f32260a3e888a3851ed09d8467c
mabar84 <mabar@tut.by>
2024-04-28 23:10:08 +0300

    button

--- src/components/auth/NewPasswordForm/NewPasswordFormValues.ts
+++
@@ -1,5 +0,0 @@
-import { z } from 'zod'
-
-import { NewPasswordFormSchema } from './NewPasswordFormShema'
-
-export type NewPasswordFormValues = z.infer<typeof NewPasswordFormSchema>
--- src/components/profile/PersonalInformation/EditNickNameForm/index.ts
+++
@@ -1,1 +0,0 @@
-export { EditNickNameForm } from './EditNickNameForm'
